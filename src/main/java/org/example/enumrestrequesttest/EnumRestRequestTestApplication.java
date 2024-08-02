7ce1a0df78ed15eb36116ec0efd50aa769967b46
Denis <denis.glod@mail.ru>
2024-08-03 01:15:34 +0300

    init

---
+++ src/main/java/org/example/enumrestrequesttest/EnumRestRequestTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.enumrestrequesttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumRestRequestTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnumRestRequestTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/enumrestrequesttest/EnumRestRequestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.enumrestrequesttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnumRestRequestTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
